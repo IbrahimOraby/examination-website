96c3635121ca9c5f0edcd065a50b93370eb63855
AlaaMatouk <contact.alaamatouk@gmail.com>
2025-05-05 20:33:45 +0300

    added exams and score pages

---
+++ assets/js/welcomeee-page.js
@@ -0,0 +1,17 @@
+// let welcomeName = document.getElementById("welcome-name");
+// let signOutBtn = document.getElementById("signup-btn");
+// let examsBtn = document.getElementById("exams-btn");
+// let homeTap = document.getElementById("home-tap");
+// let dashTap = document.getElementById("dash-tap");
+// let examsTap = document.getElementById("exams-tap");
+// let scoreTap = document.getElementById("score-tap");
+
+// examsBtn.addEventListener("click", () => {
+//   location.href = "/pages/exams.html";
+// });
+// examsTap.addEventListener("click", () => {
+//   location.href = "/pages/exams.html";
+// });
+// scoreTap.addEventListener("click", () => {
+//   location.href = "/pages/score.html";
+// });
