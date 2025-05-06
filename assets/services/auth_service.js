0bced5806b2bfe645e68d666250b1da8bd3346a7
IbrahimOraby <orabyibrahim19@gmail.com>
2025-05-07 02:57:39 +0300

    fix: Remove duplication

--- assets/services/auth_service.js
+++ assets/services/auth_service.js
@@ -1,29 +1,14 @@
 import {
-  auth,
-  createUserWithEmailAndPassword,
-  updateProfile,
-  signInWithEmailAndPassword,
+	auth,
+	createUserWithEmailAndPassword,
+	updateProfile,
+	signInWithEmailAndPassword
 } from "../../firebase.js";
 import { loginError } from "../js/login.js";
 import { clearLoginErrors } from "../js/login.js";
 
 // Sign up
 export const userSignup = async (userName, email, password) => {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    console.log(userCredential);
-    await updateProfile(userCredential.user, {
-      displayName: userName,
-    });
-    console.log(`${userCredential.user.email} signed up`);
-    return userCredential;
-  } catch (error) {
-    throw error; // Re-throw the error if you want calling code to handle it
-  }
 	try {
 		const userCredential = await createUserWithEmailAndPassword(
 			auth,
@@ -35,7 +20,7 @@ export const userSignup = async (userName, email, password) => {
 			displayName: userName
 		});
 		console.log(`${userCredential.user.email} signed up`);
-		return userCredential.user;
+		return userCredential;
 	} catch (error) {
 		throw error; // Re-throw the error if you want calling code to handle it
 	}
@@ -43,18 +28,18 @@ export const userSignup = async (userName, email, password) => {
 
 // sign in
 export const userSignin = async (email, password) => {
-  try {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    console.log("Logged in:");
+	try {
+		const userCredential = await signInWithEmailAndPassword(
+			auth,
+			email,
+			password
+		);
+		console.log("Logged in:");
 
-    return userCredential;
-  } catch (error) {
-    throw error;
-    // loginError("", "Invalid email or password. Please try again.");
-    // console.log(error);
-  }
+		return userCredential;
+	} catch (error) {
+		throw error;
+		// loginError("", "Invalid email or password. Please try again.");
+		// console.log(error);
+	}
 };
