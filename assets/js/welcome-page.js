0a7cd055683168165d978ae4b68d4853e18b4c54
IbrahimOraby <orabyibrahim19@gmail.com>
2025-05-07 03:20:56 +0300

    feat: Implement signout functionality in welcome-page

--- assets/js/welcome-page.js
+++ assets/js/welcome-page.js
@@ -1,16 +1,28 @@
 import { getAuth, onAuthStateChanged } from "../../firebase.js";
+import { signoutUser } from "../services/auth_service.js";
 const auth = getAuth();
 const user = auth.currentUser;
-let userNameSpan = document.getElementById("userName");
+const userNameSpan = document.getElementById("userName");
+const sigoutBtn = document.getElementById("signout-btn");
 
 onAuthStateChanged(auth, (user) => {
 	if (user) {
 		// User is signed in
-		console.log(user);
-        const fullName = user.displayName;
-        const firstName = fullName.split(" ")[0];
+		const fullName = user.displayName;
+		const firstName = fullName.split(" ")[0];
 		userNameSpan.textContent = firstName;
 	} else {
 		console.log("No user signed in");
 	}
 });
+
+sigoutBtn.addEventListener("click", handleSignout);
+
+async function handleSignout() {
+	try {
+		await signoutUser();
+		window.location.replace("../../index.html");
+	} catch (error) {
+		console.log("Error while logging out", error);
+	}
+}
