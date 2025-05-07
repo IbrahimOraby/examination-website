8d81e5e690d3463f7895a3ae9ea55738e208873f
AlaaMatouk <contact.alaamatouk@gmail.com>
2025-05-07 12:57:46 +0300

    added timer

---
+++ assets/js/timer.js
@@ -0,0 +1,28 @@
+var countDownDate = new Date().getTime() + 25 * 60 * 1000;
+
+function formatTime(unit) {
+  return unit < 10 ? "0" + unit : unit;
+}
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  if (distance <= 5 * 60000) {
+    document.getElementById("timer").style.color = "red";
+  } else {
+    document.getElementById("timer").style.color = "black";
+  }
+
+  document.getElementById("timer").innerHTML =
+    formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds);
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
