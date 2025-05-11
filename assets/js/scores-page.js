71bd4e0f2bb1759a077b658ac8760f1375497d1b
IbrahimOraby <orabyibrahim19@gmail.com>
2025-05-11 22:55:39 +0300

    style: display results for the student

--- assets/js/scores-page.js
+++ assets/js/scores-page.js
@@ -1,89 +1,80 @@
-// import { getExamsResultsData } from "../services/firestore_service.js";
-// import Exam from "./Exam.js";
-// const uid = localStorage.getItem("uid");
+import { getExamsResultsData } from "../services/firestore_service.js";
+import handleSignout from "./handleSingout.js";
 
-// function showLoading() {
-// 	const tbody = document.querySelector("tbody");
-// 	tbody.innerHTML = `
-//         <tr class="loading-row">
-//             <td colspan="4">
-// 			<div class='d-flex align-items-center justify-content-center my-1'>	
-//                 <div class="loader"></div>
-// 			</div>
-//             </td>
-//         </tr>
-//     `;
-// }
+const uid = localStorage.getItem("uid");
+const sigoutBtn = document.getElementById("signout-btn");
 
-// class ExamResult {
-// 	constructor(id, uid, userScore, subjectId) {
-// 		this.id = id;
-// 		this.uid = uid;
-// 		this.userScore = userScore;
-// 		this.subjectId = subjectId;
-// 	}
-// }
+function showLoading() {
+	const tbody = document.querySelector("tbody");
+	tbody.innerHTML = `
+        <tr class="loading-row">
+            <td colspan="4">
+			<div class='d-flex align-items-center justify-content-center my-1'>	
+                <div class="loader"></div>
+			</div>
+            </td>
+        </tr>
+    `;
+}
 
-// async function loadExamsResults() {
-// 	const examResultData = await getExamsResultsData(uid);
-// 	return examResultData.map(
-// 		(data) =>
-// 			new ExamResult(
-// 				data.id,
-// 				data.uid,
-// 				data.userScore,
-// 				data.subjectId,
-// 			)
-// 	);
-// }
+class ExamResult {
+	constructor(id, uid, userScore, subjectId, subjectTitle) {
+		this.id = id;
+		this.uid = uid;
+		this.userScore = userScore;
+		this.subjectId = subjectId;
+		this.subjectTitle = subjectTitle;
+	}
+}
 
-// const loadExamsResultsTableData = async () => {
-// 	try {
-// 		showLoading();
-// 		const examsResultsArr = await loadExamsResults();
-// 		console.log(examsResultsArr);
-// 		// const tbody = document.querySelector("tbody");
+async function loadExamsResults() {
+	const examResultData = await getExamsResultsData(uid);
+	return examResultData.map(
+		(data) =>
+			new ExamResult(
+				data.id,
+				data.uid,
+				data.userScore,
+				data.subjectId,
+				data.subjectTitle
+			)
+	);
+}
 
-// 		// // reset
-// 		// tbody.innerHTML = "";
+const loadExamsResultsTableData = async () => {
+	try {
+		showLoading();
+		const examsResultsArr = await loadExamsResults();
+		console.log(examsResultsArr);
+		const tbody = document.querySelector("tbody");
 
-// 		// examsArr.forEach((exam, index) => {
-// 		// 	const tr = document.createElement("tr");
+		// reset
+		tbody.innerHTML = "";
 
-// 		// 	// No.
-// 		// 	const noTd = document.createElement("td");
-// 		// 	noTd.textContent = `#${index + 1}`;
-// 		// 	tr.appendChild(noTd);
+		examsResultsArr.forEach((result, index) => {
+			const tr = document.createElement("tr");
 
-// 		// 	// Exam title
-// 		// 	const examTd = document.createElement("td");
-// 		// 	examTd.textContent = exam.subjectTitle;
-// 		// 	tr.appendChild(examTd);
+			// No.
+			const noTd = document.createElement("td");
+			noTd.textContent = `#${index + 1}`;
+			tr.appendChild(noTd);
 
-// 		// 	// // Question (placeholder - you might want to count questions)
-// 		// 	// const questionTd = document.createElement("td");
-// 		// 	// questionTd.textContent = "#"; // Or actual count if available
-// 		// 	// tr.appendChild(questionTd);
+			// Exam title
+			const examTd = document.createElement("td");
+			examTd.textContent = result.subjectTitle;
+			tr.appendChild(examTd);
 
-// 		// 	// Time
-// 		// 	const timeTd = document.createElement("td");
-// 		// 	timeTd.textContent = `${exam.duration} mins`;
-// 		// 	tr.appendChild(timeTd);
+			//Score
+			const scoreTd = document.createElement("td");
+			scoreTd.textContent = result.userScore + "%";
+			tr.appendChild(scoreTd);
 
-// 		// 	// Start
-// 		// 	const startTd = document.createElement("td");
-// 		// 	const startButton = document.createElement("button");
-// 		// 	startButton.textContent = "▶";
-// 		// 	startButton.className = "start-exam-btn";
-// 		// 	startButton.addEventListener("click", () => startExam(exam.id));
-// 		// 	startTd.appendChild(startButton);
-// 		// 	tr.appendChild(startTd);
+			tbody.appendChild(tr);
+		});
+	} catch (error) {
+		console.error("Error loading exam data:", error);
+	}
+};
 
-// 		// 	tbody.appendChild(tr);
-// 		// });
-// 	} catch (error) {
-// 		console.error("Error loading exam data:", error);
-// 	}
-// };
-
-// window.addEventListener("load", loadExamsResultsTableData);
+window.addEventListener("load", loadExamsResultsTableData);
+sigoutBtn.addEventListener("click", handleSignout);
