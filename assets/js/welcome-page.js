3971f63fbaff02ff91fef00bb710781231450e1a
AlaaMatouk <contact.alaamatouk@gmail.com>
2025-05-12 18:53:20 +0300

    fixed sign out btn

--- assets/js/welcome-page.js
+++ assets/js/welcome-page.js
@@ -1,21 +1,20 @@
 import handleSignout from "./handleSingout.js";
-
 const userNameSpan = document.getElementById("userName");
 const sigoutBtn = document.getElementById("signout-btn");
 
 onAuthStateChanged(auth, (user) => {
-	if (user) {
-		// User is signed in
-		const fullName = user.displayName;
-		const firstName = fullName.split(" ")[0];
-		userNameSpan.textContent = firstName;
+  if (user) {
+    // User is signed in
+    const fullName = user.displayName;
+    const firstName = fullName.split(" ")[0];
+    userNameSpan.textContent = firstName;
 
-		//set user id & user name in localstorage
-		localStorage.setItem("uid", user.uid);
-		localStorage.setItem("userName", user.displayName);
-	} else {
-		console.log("No user signed in");
-	}
+    //set user id & user name in localstorage
+    localStorage.setItem("uid", user.uid);
+    localStorage.setItem("userName", user.displayName);
+  } else {
+    console.log("No user signed in");
+  }
 });
 
 sigoutBtn.addEventListener("click", handleSignout);
